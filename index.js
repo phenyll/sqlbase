206c587942fafc5d815ad6cd769a705e61a4b033
Oliver Möller <om@batix.com>
2018-03-22 20:43:03 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const ODBC = require('odbc');
 
-function sqlbaseConnectionWrapper(initConnectionString){
-    let connectionString = initConnectionString;
-
+function sqlbaseConnectionWrapper(connectionString){
     return (sql) => {
         return (callback) => {
-            let db = ODBC();
-            db.open(connectionString, function (err, db) {
-                db.query(sql, function(err, rows/*, more*/){
-                    if(err) return err;
-                    if(rows && typeof callback === "function"){
-                        callback(rows);
+            ODBC.open(connectionString, function (err, db) {
+                if(err) throw err;
+                else db.query(sql, function(err, rows/*, more*/){
+                    if(err) throw err;
+                    else if(rows && typeof callback === "function"){
+                        callback(rows, ()=>{
+                            db.close();
+                        });
                     }
                 });
             });
